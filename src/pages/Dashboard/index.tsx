71285ec5ea2b3f28c61c55786f1ba62d858ad74e
Derkian Souza <derkian@iands.com.br>
2020-12-05 19:39:37 -0300

    feat: Inclusão e estilização de componentes de formulário e lista de repositórios

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,70 @@
 import React from 'react';
-import { Title } from './styles';
+import { FiChevronRight } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Github</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github explorer" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars3.githubusercontent.com/u/1408208?s=460&u=74f537e9da8416a236f4d3efa07d2cf61662956e&v=4"
+            alt="Derkian Afonso"
+          />
+          <div>
+            <strong>iandark/GoStack-NodeJS-Desafio</strong>
+            <p>
+              Aplicação em NodeJS que permite armazenar repositórios de um
+              portfólio
+            </p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars3.githubusercontent.com/u/1408208?s=460&u=74f537e9da8416a236f4d3efa07d2cf61662956e&v=4"
+            alt="Derkian Afonso"
+          />
+          <div>
+            <strong>iandark/GoStack-NodeJS-Desafio</strong>
+            <p>
+              Aplicação em NodeJS que permite armazenar repositórios de um
+              portfólio
+            </p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars3.githubusercontent.com/u/1408208?s=460&u=74f537e9da8416a236f4d3efa07d2cf61662956e&v=4"
+            alt="Derkian Afonso"
+          />
+          <div>
+            <strong>iandark/GoStack-NodeJS-Desafio</strong>
+            <p>
+              Aplicação em NodeJS que permite armazenar repositórios de um
+              portfólio
+            </p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
